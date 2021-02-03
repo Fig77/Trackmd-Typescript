8421d261ac760df923f724d5205eebc3dafa942e
fig77 <fig.igle@gmail.com>
2021-02-03 12:21:33 -0300

    Added default type for input fn form container, adding inputs gen

---
+++ src/container/Form/FormContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputForm from '../../components/Form/input';
+
+interface formParts {
+  inputFields: [];
+}
+
+const FormContainer = () => {
+  return <div></div>
+}
+
+export default FormContainer;
