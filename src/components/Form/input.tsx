eaac1598a899b3a250b09866edf2ccf9de56d60a
fig77 <fig.igle@gmail.com>
2021-02-02 23:11:59 -0300

    Added input component

---
+++ src/components/Form/input.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type inputFn = (e: React.FormEvent<HTMLInputElement>, i: number) => void;
+
+
+interface inputData {
+  labelText: string;
+  inputFunction: inputFn;
+  i: number;
+  fatherId: string;
+  fromType: string;
+
+}
+
+const InputForm = ({fatherId, labelText, fromType, i, inputFunction}: inputData) => { 
+
+  return(
+    <div key={fatherId.concat(i)}>
+      <label className="" >{labelText}</label>
+      <div className="">
+       <input type={fromType} className="" onChange={(e) => inputFunction(e,i)} required/>
+    </div>
+    </div>
+  );
+}
+
+export default InputForm;
