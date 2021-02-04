5bfe2e3472b4f292bfa8e33bf4a8d8f4cc10144f
fig77 <fig.igle@gmail.com>
2021-02-03 21:33:43 -0300

    Adding default login container functionality

--- src/container/Login.tsx
+++ src/container/Login.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import FormContainer from './Form/FormContainer';
 
-
 const Login = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const inputs = ['username', 'password'];
 
   const handleSubmot = ():void => {
-
+    // API call;
   };
 
   return(
@@ -16,5 +15,5 @@ const Login = () => {
     <h1>Welcome</h1>
     <div></div>
     <p>Don't you have an account? <span><a href='/'>Create account</a></span></p>
-  </div>);
+   </div>);
 }
